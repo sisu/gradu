201a5fb888c30ab0bb451fb1a8bef01a40da0ddf
Mikko Sysikaski <mikko.sysikaski@gmail.com>
2018-08-28 19:01:16 +0900

    fix indent

--- code/overlap.hpp
+++ code/overlap.hpp
@@ -13,96 +13,96 @@ template<int D>
 inline vector<pair<int,int>> overlappingBoxes(
 		const vector<Box<D>>& bs1,
 		const vector<Box<D>>& bs2) {
-  vector<int> zs;
-  for(const auto& box: bs1) {
-    zs.push_back(box[D-1].from);
-    zs.push_back(box[D-1].to);
-  }
-  sortUnique(zs);
-  vector<Box<D-1>> sub1, sub2;
-  vector<int> idx1, idx2;
+	vector<int> zs;
+	for(const auto& box: bs1) {
+		zs.push_back(box[D-1].from);
+		zs.push_back(box[D-1].to);
+	}
+	sortUnique(zs);
+	vector<Box<D-1>> sub1, sub2;
+	vector<int> idx1, idx2;
 	vector<pair<int,int>> conns;
-  for(int z: zs) {
-    sub1.clear(), sub2.clear(), idx1.clear(), idx2.clear();
-    for(size_t i=0; i<bs1.size(); ++i) {
-      if (bs1[i][D-1].contains(z)) {
-        sub1.push_back(bs1[i].project());
-        idx1.push_back(i);
-      }
-    }
-    for(size_t i=0; i<bs2.size(); ++i) {
-      if (bs2[i][D-1].contains(z)) {
-        sub2.push_back(bs2[i].project());
-        idx2.push_back(i);
-      }
-    }
-    for(auto p : overlappingBoxes(sub1, sub2)) {
-      conns.emplace_back(idx1[p.first], idx2[p.second]);
-    }
-  }
+	for(int z: zs) {
+		sub1.clear(), sub2.clear(), idx1.clear(), idx2.clear();
+		for(size_t i=0; i<bs1.size(); ++i) {
+			if (bs1[i][D-1].contains(z)) {
+				sub1.push_back(bs1[i].project());
+				idx1.push_back(i);
+			}
+		}
+		for(size_t i=0; i<bs2.size(); ++i) {
+			if (bs2[i][D-1].contains(z)) {
+				sub2.push_back(bs2[i].project());
+				idx2.push_back(i);
+			}
+		}
+		for(auto p : overlappingBoxes(sub1, sub2)) {
+			conns.emplace_back(idx1[p.first], idx2[p.second]);
+		}
+	}
 	return conns;
 }
 
 inline pair<int,int> makePair(int a, int b, bool swap) {
-  return swap ? make_pair(b, a) : make_pair(a,b);
+	return swap ? make_pair(b, a) : make_pair(a,b);
 }
 
 template<>
 inline vector<pair<int,int>> overlappingBoxes(
 		const vector<Box<2>>& bs1,
 		const vector<Box<2>>& bs2) {
-  constexpr int X_AXIS = 0;
-  constexpr int Y_AXIS = 1;
+	constexpr int X_AXIS = 0;
+	constexpr int Y_AXIS = 1;
 	vector<pair<int,int>> conns;
-  struct Event {
-    int index = -1;
-    int pos = -1;
-    bool first = false;
-    bool start = false;
-    bool operator<(const Event& e) const {
-      if (pos != e.pos) return pos < e.pos;
-      return start < e.start;
-    }
-  };
-  vector<Event> events;
-  for(int i=0; i<(int)bs1.size(); ++i) {
-    Range range = bs1[i][Y_AXIS];
-    events.push_back({i, range.from, true, true});
-    events.push_back({i, range.to, true, false});
-  }
-  for(int i=0; i<(int)bs2.size(); ++i) {
-    Range range = bs2[i][Y_AXIS];
-    events.push_back({i, range.from, false, true});
-    events.push_back({i, range.to, false, false});
-  }
-  sort(events.begin(), events.end());
+	struct Event {
+		int index = -1;
+		int pos = -1;
+		bool first = false;
+		bool start = false;
+		bool operator<(const Event& e) const {
+			if (pos != e.pos) return pos < e.pos;
+			return start < e.start;
+		}
+	};
+	vector<Event> events;
+	for(int i=0; i<(int)bs1.size(); ++i) {
+		Range range = bs1[i][Y_AXIS];
+		events.push_back({i, range.from, true, true});
+		events.push_back({i, range.to, true, false});
+	}
+	for(int i=0; i<(int)bs2.size(); ++i) {
+		Range range = bs2[i][Y_AXIS];
+		events.push_back({i, range.from, false, true});
+		events.push_back({i, range.to, false, false});
+	}
+	sort(events.begin(), events.end());
 
-  struct Item {
-    int end = -1;
-    int index = -1;
-  };
-  std::map<int, Item> map1, map2;
+	struct Item {
+		int end = -1;
+		int index = -1;
+	};
+	std::map<int, Item> map1, map2;
 
-  for(Event event: events) {
-    auto& items = event.first ? map1 : map2;
-    const auto& bs = event.first ? bs1 : bs2;
-    Range range = bs[event.index][X_AXIS];
-    if (!event.start) {
-      items.erase(range.from);
-      continue;
-    }
-    items[range.from] = {range.to, event.index};
-    const auto& others = event.first ? map2 : map1;
-    auto it = others.lower_bound(range.from);
-    if (it != others.begin()) {
-      auto x = prev(it);
-      if (x->second.end > range.from) {
-        conns.push_back(makePair(event.index, x->second.index, !event.first));
-      }
-    }
-    for(; it != others.end() && it->first < range.to; ++it) {
-      conns.push_back(makePair(event.index, it->second.index, !event.first));
-    }
-  }
-  return conns;
+	for(Event event: events) {
+		auto& items = event.first ? map1 : map2;
+		const auto& bs = event.first ? bs1 : bs2;
+		Range range = bs[event.index][X_AXIS];
+		if (!event.start) {
+			items.erase(range.from);
+			continue;
+		}
+		items[range.from] = {range.to, event.index};
+		const auto& others = event.first ? map2 : map1;
+		auto it = others.lower_bound(range.from);
+		if (it != others.begin()) {
+			auto x = prev(it);
+			if (x->second.end > range.from) {
+				conns.push_back(makePair(event.index, x->second.index, !event.first));
+			}
+		}
+		for(; it != others.end() && it->first < range.to; ++it) {
+			conns.push_back(makePair(event.index, it->second.index, !event.first));
+		}
+	}
+	return conns;
 }
--- code/overlapTest.cpp
+++ code/overlapTest.cpp
@@ -8,35 +8,35 @@ using ::testing::UnorderedElementsAre;
 using namespace std;
 
 Box<2> box2(Range x, Range y) {
-  return {{x,y}};
+	return {{x,y}};
 }
 
 Box<3> box3(Range x, Range y, Range z) {
-  return {{x,y,z}};
+	return {{x,y,z}};
 }
 
 TEST(OverlapTest2D, Simple) {
-  vector<Box<2>> bs1 = {
-    box2({0,2}, {0,1}),
-    box2({1,3}, {0,2})};
-  vector<Box<2>> bs2 = {
-    box2({0,1}, {0,2}),
-    box2({1,3}, {0,2})};
-  EXPECT_THAT(overlappingBoxes(bs1, bs2),
-      UnorderedElementsAre(
-        make_pair(0,0), make_pair(0,1), make_pair(1,1)));
+	vector<Box<2>> bs1 = {
+		box2({0,2}, {0,1}),
+		box2({1,3}, {0,2})};
+	vector<Box<2>> bs2 = {
+		box2({0,1}, {0,2}),
+		box2({1,3}, {0,2})};
+	EXPECT_THAT(overlappingBoxes(bs1, bs2),
+			UnorderedElementsAre(
+				make_pair(0,0), make_pair(0,1), make_pair(1,1)));
 }
 
 TEST(OverlapTest3D, Simple) {
-  vector<Box<3>> bs1 = {
-    box3({0,2}, {0,1}, {0,1}),
-    box3({1,3}, {0,2}, {0,2})};
-  vector<Box<3>> bs2 = {
-    box3({0,1}, {0,2}, {0,1}),
-    box3({1,3}, {0,2}, {1,2})};
-  EXPECT_THAT(overlappingBoxes(bs1, bs2),
-      UnorderedElementsAre(
-        make_pair(0,0), make_pair(1,1)));
+	vector<Box<3>> bs1 = {
+		box3({0,2}, {0,1}, {0,1}),
+		box3({1,3}, {0,2}, {0,2})};
+	vector<Box<3>> bs2 = {
+		box3({0,1}, {0,2}, {0,1}),
+		box3({1,3}, {0,2}, {1,2})};
+	EXPECT_THAT(overlappingBoxes(bs1, bs2),
+			UnorderedElementsAre(
+				make_pair(0,0), make_pair(1,1)));
 }
 
 } // namespace
